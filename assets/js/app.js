b053e4d33235e89376399e9c2d2e5969af91eea7
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-19 10:43:22 -0500

    Primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+window.addEventListener( 'load', function (e){
+	e.preventDefault();
+
+	var contenedor = document.getElementById('contenedor');
+	
+	for (var i = 1; i <= 6; i++){
+
+		var img = document.createElement('img');
+		var span = document.createElement('span');
+		var figure = document.createElement('figure');
+
+		var coders = 	['Rossemary Madonado','Nadia Cuadros','Katherine Ortega',
+						'Flor Tello','Mitchell Rodríguez','Karin Alejo'];
+
+
+	 }
+})
