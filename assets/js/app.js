ce0881998343977b47437a5991a3b8048f631993
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-19 12:54:02 -0500

    Colocando imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,41 @@ window.addEventListener( 'load', function (e){
 	e.preventDefault();
 
 	var contenedor = document.getElementById('contenedor');
-	
+	/*
+	var section = document.createElement('section');
+	var titulo = document.createElement('h2');
+	var linea = document.createElement('hr');
+
+	titulo.innerText = "Nuestras coders";
+	titulo.appendChild(contenedor);
+
+	span.setAttribute('class','linea');
+	contenedor.appendChild(span);
+	*/
+	var coders = 	['Rossemary Madonado','Nadia Cuadros','Katherine Ortega',
+					'Flor Tello','Mitchell Rodríguez','Karin Alejo'];
+
 	for (var i = 1; i <= 6; i++){
 
-		var img = document.createElement('img');
-		var span = document.createElement('span');
+		var urlImg = 'assets/img/'+ i +'.png';
+		var div = document.createElement('div');
 		var figure = document.createElement('figure');
+		var img = document.createElement('img');
+		var figcaption = document.createElement('figcaption');
+		var span = document.createElement('span');
 
-		var coders = 	['Rossemary Madonado','Nadia Cuadros','Katherine Ortega',
-						'Flor Tello','Mitchell Rodríguez','Karin Alejo'];
+		img.setAttribute('src', urlImg);
+		img.setAttribute('class','img');
 
+		figure.appendChild(img);
+		figure.setAttribute('class','imagenes');
 
+		span.innerText = coders[i];
+		span.setAttribute('class','nombre-coder');
+		figcaption.appendChild(span);
+		figure.appendChild(figcaption);
+
+		div.appendChild(figure);
+		contenedor.appendChild(figure);
 	 }
 })
